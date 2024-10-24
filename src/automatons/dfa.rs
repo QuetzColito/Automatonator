304f8fccabe69125408ac22ed58fb2fdbe517054
Stefan Lahne <stefan.lahne@proton.me>
2024-10-24 18:48:33 +0200

    modularize

---
+++ src/automatons/dfa.rs
@@ -0,0 +1,56 @@
+use crate::automatons::shared::*;
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+pub struct DFA {
+    states: HashMap<VertexId, HashMap<char, VertexId>>,
+    final_states: HashSet<VertexId>,
+    start_state: VertexId,
+}
+
+impl Automaton for DFA {
+    fn accepts(&self, word: &str) -> bool {
+        let mut current = &self.start_state;
+        let mut encountered_missing_edge = false;
+        word.chars().for_each(|symbol: char| {
+            if let Some(next) = self.states.get(current).unwrap().get(&symbol) {
+                current = next;
+            } else {
+                encountered_missing_edge = true;
+            }
+        });
+        self.final_states.contains(current) && !encountered_missing_edge
+    }
+}
+
+impl DFA {
+    pub fn new(data: impl Iterator<Item = AutomatonData>) -> DFA {
+        let mut states = HashMap::new();
+        let mut final_states = HashSet::new();
+        let mut start_state = "".to_string();
+        data.for_each(|d| match d {
+            AutomatonData::Edge(source, target, label) => {
+                let label = label.parse::<char>().unwrap_or('e');
+                states
+                    .entry(source)
+                    .or_insert(HashMap::new())
+                    .insert(label, target);
+            }
+            AutomatonData::Final(id) => {
+                final_states.insert(id);
+            }
+            AutomatonData::Start(id) => {
+                if start_state != "" {
+                    println!("multiple start states in a dfa, overwriting")
+                };
+                start_state = id;
+            }
+        });
+        assert_ne!(start_state, "", "No start state given");
+        DFA {
+            states,
+            final_states,
+            start_state,
+        }
+    }
+}
