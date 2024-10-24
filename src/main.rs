17b488db8bd427e49655045c254b43c17c008987
Stefan Lahne <stefan.lahne@proton.me>
2024-10-24 13:13:08 +0200

    add xml parsing

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,176 @@
+use core::panic;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::env;
 use std::fs;
 
+type VertexId = String;
+
+enum AutomatonData {
+    Edge(VertexId, VertexId, String),
+    Final(VertexId),
+    Start(VertexId),
+}
 
 trait Automaton {
-    fn accepts(&self, word:&str) -> bool;
+    fn accepts(&self, word: &str) -> bool;
 }
 
 struct DFA {
-    states: Vec<HashMap<char, usize>>,
-    final_states: HashSet<usize>,
-    start_state: usize
+    states: HashMap<VertexId, HashMap<char, VertexId>>,
+    final_states: HashSet<VertexId>,
+    start_state: VertexId,
 }
 
-impl Automaton for DFA {
-    fn accepts(&self, word:&str) -> bool {
-        let mut current = self.start_state;
-        let mut encountered_missing_edge = false;
-        word.chars().for_each(|symbol:char| {
-            if let Some(next) = self.states[current].get(&symbol) {
-                current = *next;
-            } else {
-                encountered_missing_edge = true;
+impl DFA {
+    fn new(data: impl Iterator<Item = AutomatonData>) -> DFA {
+        let mut states = HashMap::new();
+        let mut final_states = HashSet::new();
+        let mut start_state = "".to_string();
+        data.for_each(|d| match d {
+            AutomatonData::Edge(source, target, label) => {
+                let label = label.parse::<char>().unwrap_or('e');
+                states
+                    .entry(source)
+                    .or_insert(HashMap::new())
+                    .insert(label, target);
+            }
+            AutomatonData::Final(id) => {
+                final_states.insert(id);
+            }
+            AutomatonData::Start(id) => {
+                if start_state != "" {
+                    println!("multiple start states in a dfa, overwriting")
+                };
+                start_state = id;
             }
         });
-        self.final_states.contains(&current) && !encountered_missing_edge
-    }
-}
-
-fn parse_text_automaton(file:String) -> Box<dyn Automaton> {
-    let mut lines = file.lines();
-    loop {
-        let line = lines.next().expect("no type line found");
-        let mut values = line.split_whitespace();
-        if let Some(value) = values.next() {
-            if value == "t" {
-                return match values.next().expect("no actual type given in type line") {
-                    "DFA" => {
-                        let vertices = values.next().expect("missing vertices count")
-                            .parse::<usize>().expect("vertices count not a number");
-                        Box::new(parse_text_dfa(file, vertices))
-                    },
-                    _ => unimplemented!()
-                }
-            }
+        assert_ne!(start_state, "", "No start state given");
+        DFA {
+            states,
+            final_states,
+            start_state,
         }
     }
 }
 
-fn parse_text_dfa(file:String, vertices:usize) -> DFA {
-        let mut automat = DFA {
-            states: vec![HashMap::new(); vertices],
-            final_states: HashSet::new(),
-            start_state: 0,
-        };
-        file.lines().for_each(|line: &str| {
-            println!("{line}");
-            let mut values = line.split_whitespace();
-            if let Some(value) = values.next() {
-                match value {
-                    "c" => (), // Comment, do nothing
-                    "t" => (), // type is already covered, do nothing
-                    "s" => {
-                        automat.start_state = values.next().unwrap().parse::<usize>()
-                            .expect("start state is not a number") - 1;
-                    },
-                    "f" => {
-                        automat.final_states.insert(values.next().unwrap().parse::<usize>()
-                            .expect("final state is not a number"));
-                    },
-                    _ => {
-                        if let Ok(source) = value.parse::<usize>() {
-                            let target = values.next().unwrap().parse::<usize>()
-                                .expect("start state is not a number");
-                            let label = values.next().unwrap().parse::<char>().unwrap_or('e');
-                            automat.states[source - 1].insert(label, target - 1);
-                        } else {
-                            println!("ignored pattern {line}");
-                        };
+impl Automaton for DFA {
+    fn accepts(&self, word: &str) -> bool {
+        let mut current = &self.start_state;
+        let mut encountered_missing_edge = false;
+        word.chars().for_each(|symbol: char| {
+            if let Some(next) = self.states.get(current).unwrap().get(&symbol) {
+                current = next;
+            } else {
+                encountered_missing_edge = true;
+            }
+        });
+        self.final_states.contains(current) && !encountered_missing_edge
+    }
+}
+
+fn parse_text_automaton(file: String) -> Box<dyn Automaton> {
+    let mut lines = file.lines();
+    let mut arguments = lines
+        .find(|value| value.trim().starts_with("t"))
+        .expect("No Type Line given")
+        .split_whitespace();
+
+    arguments.next(); // drop the argument containing t
+
+    return match arguments.next().expect("no actual type given in type line") {
+        "DFA" => Box::new(parse_text_dfa(file)),
+        _ => panic!("Type Unknown"),
+    };
+}
+
+fn parse_text_dfa(file: String) -> DFA {
+    let automaton_data = file.lines().filter_map(|line: &str| {
+        println!("{line}");
+        let mut values = line.split_whitespace();
+        if let Some(value) = values.next() {
+            match value {
+                "c" | "t" => None, // type is already covered, do nothing
+                "s" => Some(AutomatonData::Start(
+                    values
+                        .next()
+                        .expect("missing start state identifier")
+                        .to_string(),
+                )),
+                "f" => Some(AutomatonData::Final(
+                    values
+                        .next()
+                        .expect("missing final state identifier")
+                        .to_string(),
+                )),
+                _ => {
+                    if let Some(target) = values.next() {
+                        Some(AutomatonData::Edge(
+                            value.to_string(),
+                            target.to_string(),
+                            values.next().unwrap_or("e").to_string(),
+                        ))
+                    } else {
+                        println!("ignored pattern {line}");
+                        None
                     }
                 }
             }
+        } else {
+            println!("empty line");
+            None
+        }
+    });
+    DFA::new(automaton_data)
+}
+
+fn parse_xml_automaton(file: String) -> Box<dyn Automaton> {
+    let data =
+        roxmltree::Document::parse(&file).expect("XML Parsing Error (roxmltree threw an Error)");
+    let automaton_data = data
+        .descendants()
+        .filter(|node| node.has_attribute("edge") || node.has_attribute("vertex"))
+        .filter_map(|node| {
+            if node.has_attribute("vertex") {
+                if node
+                    .parent()
+                    .expect("root should have been filtered out by now")
+                    .tag_name()
+                    .name()
+                    != "root"
+                {
+                    Some(AutomatonData::Final(
+                        node.parent()
+                            .unwrap()
+                            .attribute("id")
+                            .expect("final vertex without id")
+                            .to_string(),
+                    ))
+                } else {
+                    Option::None
+                }
+            } else {
+                assert!(node.has_attribute("edge"));
+                if node.has_attribute("source") && node.has_attribute("target") {
+                    Some(AutomatonData::Edge(
+                        node.attribute("source").unwrap().to_string(),
+                        node.attribute("target").unwrap().to_string(),
+                        node.attribute("value").unwrap_or("e").to_string(),
+                    ))
+                } else {
+                    Some(AutomatonData::Start(
+                        if let Some(id) = node.attribute("target") {
+                            id.to_string()
+                        } else {
+                            node.attribute("source")
+                                .expect("free floating edge")
+                                .to_string()
+                        },
+                    ))
+                }
+            }
         });
-        automat
+    Box::new(DFA::new(automaton_data))
 }
 
 fn main() {
@@ -91,11 +178,15 @@ fn main() {
     if args.len() != 2 {
         println!("No Automaton given");
     } else {
-        println!("Reading Automaton from {}", args[1]);
+        let filepath = args[1].clone();
+        println!("Reading Automaton from {}", filepath);
+        let file = fs::read_to_string(filepath.clone()).expect("file doesn't exist");
 
-        let file = fs::read_to_string(args[1].clone()).expect("file doesn't exist");
-        let automat = parse_text_automaton(file);
-
+        let automat = if filepath.ends_with(".xml") {
+            parse_xml_automaton(file)
+        } else {
+            parse_text_automaton(file)
+        };
 
         println!("{}", automat.accepts("aaa"));
         println!("{}", automat.accepts("aaaaaa"));
