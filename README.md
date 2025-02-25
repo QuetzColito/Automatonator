f9fb534988a3bd187b7bdacb435d32b6d5f89aee
Stefan Lahne <stefan.lahne@proton.me>
2025-02-25 16:51:07 +0100

    flake: add the ability to cross-compile

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ cargo run -- -h
 ```
 
 you can use `nix develop` with the provided flake to install cargo if you want
-
-Note: `--` does not work in NuShell (dunno why tho), so add a `^` at the start of the line for compatibility
--- flake.nix
+++ flake.nix
@@ -1,19 +1,33 @@
 {
-    description = "Rust flake";
-    inputs =
-        {
-            nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # or whatever vers
-        };
+  description = "Rust flake";
 
-    outputs = { self, nixpkgs, ... }@inputs:
-        let
-            system = "x86_64-linux"; # your version
-            pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-            devShells.${system}.default = pkgs.mkShell
-            {
-                packages = with pkgs; [ rustc cargo ]; # whatever you need
-            };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              pkg-config
+              (rust-bin.stable.latest.default.override {targets = ["x86_64-unknown-linux-musl"];})
+            ];
+          };
+      }
+    );
 }
